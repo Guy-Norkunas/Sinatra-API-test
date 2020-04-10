1910e9720016fcefe8d20e62e7ab1bf2fdf9ae5f
Guy Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-04-10 13:08:40 +1000

    added lots of comments to make it easier to understand

--- index.rb
+++ index.rb
@@ -3,40 +3,58 @@ require 'sinatra/json'
 require 'erb'
 require 'httparty'
 
-PORT = 9494
 
+#allows the localhost port this webserves runs in to be changed
+
+PORT = 9494
 set :port, PORT
 
-set(:query) { |val| condition { request.query_string == val } }
+#base landing page of localhost:PORT
+#redirects to the pokemon stuff so you don't have to remember URL
 
 get "/" do
   redirect "/pokemon/jynx"
 end
 
+#just in case someone doesnt type anything in url
+
 get "/pokemon" do
   redirect "/pokemon/jynx"
 end
+
+#this is triggered when a blank search is tried
+#handled by redirecting to a valid pokemon search
 
 get "/pokemon/" do
   redirect "/pokemon/jynx"
 end
 
+#:name notation allows that part of the url to be accessed
+#this value is later used for an api call
+
 get "/pokemon/:name" do
+
+  #checks if something was typed into the search bar
+  #if there was, redirects to that pokemons site
+
   if params["search"]
     redirect "/pokemon/#{params["search"].downcase}"
   end
 
-  response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/#{params["name"].downcase}")
+  #gets the info form pokeapi and parses it into a nice object
 
+  response = HTTParty.get("https://pokeapi.co/api/v2/pokemon/#{params["name"].downcase}")
   pokemon_info = response.parsed_response
 
-  p pokemon_info
+  #the following if triggers if the api pokeapi query was invalid
+  #it then redirects to a valid pokemon
 
   if pokemon_info == "Not Found"
     redirect "/pokemon/jynx"
   end
 
-  pokemon_info["sprites"]["front_default"]
+  #these @'s allows the values to be accessed by the erb file at the end
+  #and everything else is just accessing specifc parts of the pokemon_info object
 
   @sprite1 = pokemon_info["sprites"]["front_default"]
   @sprite2 = pokemon_info["sprites"]["back_default"]
@@ -46,5 +64,14 @@ get "/pokemon/:name" do
   @height = pokemon_info["height"]
   @weight = pokemon_info["weight"]
 
+  #returns the erb file to the person who triggered the GET request
+  #in a browser this will display as html/css
+
   erb :pokemon_selected
 end
+
+#planned
+
+#add shiny_sprites
+#add base_stats
+#add multiple pages that link to each other (api has good features for this)
